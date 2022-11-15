3f5a5174a900fe4ae2c7f03a450085ef3d93b732
Aaron Enyetu <aaronenyetu@gmail.com>
2022-11-15 15:56:51 -0800

    Implemented React Component Life Cycles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
+//React useEffect is a function that gets executed for 3 different React component lifecycles.
+//Those lifecycles are componentDidMount, componentDidUpdate, and componentWillUnmount lifecycles.
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [message, setMessage] = useState('Hi there, how are you?');
+
+  //But now I want to useEffect to change the message a second after the component has mounted.
+  //useEffect accepts a function as it’s first argument. This function handler will take care of any side effects you like when it gets run.
+  //The function is a callback function after one of the React component lifecycle has been triggered.
+  useEffect(() => {
+    console.log('Trigger use effect hook');
+
+    setTimeout(() => {
+      setMessage("I'm fine, thanks for asking.");
+    }, 1000);
+  }, []);
+
+  return <h1>{message}</h1>; //grabbing my state variable, message, and I’m printing it to the screen.
 }
 
 export default App;
